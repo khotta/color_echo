86a9df15047cbbc647b6428637cc72dea670fa2d
khotta <khotta116@gmail.com>
2017-01-25 21:20:44 +0900

    update to 3.1.1

--- lib/color_echo/const.rb
+++ lib/color_echo/const.rb
@@ -1,6 +1,6 @@
 module CE
     LIBS_NAME   = "color_echo"
-    VERSION     = "3.1.0"
+    VERSION     = "3.1.1"
     SUMMARY     = "Decorates command line output with ANSI escape sequence."
     HOMEPAGE    = "http://colorecho.github.io"
     DESCRIPTION = %(#{SUMMARY} #{HOMEPAGE})
